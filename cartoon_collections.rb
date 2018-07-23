1c7d0792475d3b2da43541124e9cf8c9356ffd07
Sheena Bonilla <sheena.myb@gmail.com>
2018-07-23 02:01:45 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -41,7 +41,7 @@ array = []
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   cheese_types.each do |el|
-    if cheese_types.include?(el)
+    if array.include?(el)
       puts el
 end
 end
